f62095a2abee38007d228befd5ee69c54e1fe254
guidoop <122056111+guidoop@users.noreply.github.com>
2023-01-20 16:48:58 -0300

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'da045852bad6428f95dd40723119fefa',
+  rootNotionPageId: 'e161839e5ef446ee8622fd323022ed45', 
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
